7675cca21e531f75f664f247963bda80064bfe41
Carlos Mantilla <ceo@aliongroo.com>
2015-06-20 13:31:58 +0300

    Merge master into 1.

--- src/aurelia-leaflet.js
+++ src/aurelia-leaflet.js
@@ -1,12 +1,20 @@
 import L from 'leaflet';
 
 export class AureliaLeaflet {
-
   constructor() {
-    //this.l = L;
-    this.model = {
-      geo: 'this.LatLng',
-      zoom: 15
-    };
+    this.L = L;
   }
+
+  attached() {
+    __createMap({
+      center: [51.505, -0.09],
+      zoom: 14
+    });
+  }
+
+  __createMap(options) {
+    this.map = this.L.map('map', options);
+  }
+
 }
+
